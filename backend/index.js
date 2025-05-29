0bb5a1e18043a7475076f72a4d2caa1fb86f5efc
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-05-29 16:19:26 +0530

    added frontend url in backend cors

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import parseRouter from './routes/parse.js';
+import parseRouter from "./routes/parse.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://resume-parser-demo.vercel.app/"],
+  })
+);
 
 const PORT = process.env.PORT | 5050;
 
-app.use("/api",parseRouter)
+app.use("/api", parseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
